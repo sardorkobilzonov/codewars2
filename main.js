b9bfb3d6a4ecc194d5285f2250d5e433daf2430f
sardorkobilzonov <sardorkobilzonov4@gmail.com>
2024-11-24 17:19:55 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,57 @@
+//1-masala
+
+// function greet(name) {
+//     return `hello world!`;
+// }
+
+//2-masala
+// function descendingOrder(n){
+//     //...
+//      return parseInt(String(n).split('').sort((a, b) => b - a).join(''), 10);
+//   }
+
+//3-masala
+
+// var isSquare = function(n){
+//     return n >= 0 && Math.sqrt(n) % 1 === 0;
+//   }
+
+//4-masala
+// function greet(name) {
+//     return `hello world!`;
+// }
+
+//5-masala
+// function makeUpperCase(str) {
+//     // Code here
+//     return str.toUpperCase()
+//   }
+
+//6-masala
+// function greet(name){
+//     //your code here
+//      return `Hello, ${name} how are you doing today?`
+//   }
+
+//7-masala
+// function booleanToString(b){
+//     //your code here
+//      return b.toString()
+//   }
+
+//8-masala
+// function maps(x){
+//     return x.map(num => num * 2);
+//     }
+
+//9-masala
+// function digitize(n) {
+//     //code here
+//     return n.toString().split("").reverse().map(Number);
+//   }
+
+//10-masala
+// function filter_list(l) {
+//     // Return a new array with the strings filtered out
+//     return l.filter(item => typeof item === 'number');
+//   }
